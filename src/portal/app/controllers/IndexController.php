2542d02f581438b690e1424ff024581c43eb9f01
taohs <50584211@qq.com>
2015-09-08 01:35:48 +0800

    #bug 选择商品页面 --- 不能正确显示  商品页面； #bug 微信端 下单流程优化；我操累死我了 #bug 技师登录，提交，修改； #bug 后台修改订单时现在可以添加备注了；

--- src/portal/app/controllers/IndexController.php
+++ src/portal/app/controllers/IndexController.php
@@ -1,81 +1,89 @@
 <?php
+
 class IndexController extends ControllerBase
 {
     public function indexAction()
     {
-        $a_z=range('A','Z');
-        $brands = HdBrands::find( array( 'order' => 'initials asc' ) );
-        $this->view->setvar( 'brands', $brands );
-        $this->view->setVar('a_z',$a_z);
+        $a_z = range('A', 'Z');
+        $brands = HdBrands::find(array('order' => 'initials asc'));
+        $this->view->setvar('brands', $brands);
+        $this->view->setVar('a_z', $a_z);
     }
-    public function getautoAction(){
-        $bid=$this->request->getPost('cid');
-        $type=$this->request->getPost('method');
-        if($type==1){
+
+    public function getautoAction()
+    {
+        $bid = $this->request->getPost('cid');
+        $type = $this->request->getPost('method');
+        if ($type == 1) {
             $auto = HdAutoModels::find(array(
                 "conditions" => "brands_id = :brands_id:",
-                "bind"       => array( 'brands_id' => $bid )
+                "bind" => array('brands_id' => $bid)
             ))->toArray();
-        }else{
+        } else {
             $auto = HdAutoModelsExact::find(array(
                 "conditions" => "models_id = :models_id:",
-                "bind"       => array( 'models_id' => $bid )
+                "bind" => array('models_id' => $bid)
             ))->toArray();
         }
-        $data=$this->getJsonByArray($auto,$type);
+        $data = $this->getJsonByArray($auto, $type);
         echo json_encode($data);
         exit;
     }
-    public function getJsonByArray($auto,$type){
-        $data['status']=1;
-        $data['msg']='';
-        $i=0;
-        foreach($auto as $row){
-            $data['msg'][$i]['id']=$row['id'];
-            $data['msg'][$i]['title']=$row['name'];
-            if($type==2){
-                $data['msg'][$i]['url']="/appointment/service?brands_id={$row['brands_id']}&models_id={$row['models_id']}";
+
+    public function getJsonByArray($auto, $type)
+    {
+        $data['status'] = 1;
+        $data['msg'] = '';
+        $i = 0;
+        foreach ($auto as $row) {
+            $data['msg'][$i]['id'] = $row['id'];
+            $data['msg'][$i]['title'] = $row['name'];
+            if ($type == 2) {
+                $data['msg'][$i]['url'] = "/appointment/service?brands_id={$row['brands_id']}&models_id={$row['models_id']}";
             }
             $i++;
         }
         return $data;
     }
+
     public function loginAction()
     {
-        if ($this->session->get( 'auth' )) {
-            return $this->response->redirect( "index" );
+        if ($this->session->get('auth')) {
+            return $this->response->redirect("index");
         }
-        $reUrl = $this->request->getQuery( 'reUrl' );
+        $reUrl = $this->request->getQuery('reUrl');
         if ($this->request->isPost()) {
-            if ($this->security->checkToken( $this->session->get( '$PHALCON/CSRF/KEY$' ), $this->security->getSessionToken() )) {
-                $mobile = $this->request->getPost( 'mobile' );
-                $code = $this->request->getPost( 'code' );
+            if ($this->security->checkToken($this->session->get('$PHALCON/CSRF/KEY$'), $this->security->getSessionToken())) {
+                $mobile = $this->request->getPost('mobile');
+                $code = $this->request->getPost('code');
                 $webApi = new WebapiComponent();
-                $re=$webApi->webApiLogin($mobile,$code);
-                if($re->statusCode=='000000'){
-                    $this->session->set( 'auth', $re->content );
-                    return $this->response->redirect( $reUrl );
-                }else{
-                    $this->flash->error( $re->statusMsg );
+                $re = $webApi->webApiLogin($mobile, $code);
+                if ($re->statusCode == '000000') {
+                    $this->session->set('auth', $re->content);
+                    return $this->response->redirect($reUrl);
+                } else {
+                    $this->flash->error($re->statusMsg);
                 }
             } else {
-                $this->flash->error( "token errors" );
+                $this->flash->error("token errors");
             }
         }
     }
+
     public function getcodeAction()
     {
-        $mobile = $this->request->getPost( 'mobile' );
+        $mobile = $this->request->getPost('mobile');
         $webApi = new WebapiComponent();
-        $re = $webApi->webApiGetCode( $mobile );
+        $re = $webApi->webApiGetCode($mobile);
         echo $re;
         exit;
     }
-    function userRegister( $user, $mobile )
+
+    function userRegister($user, $mobile)
     {
-        $code = $this->session->get( 'code' );
+        $code = $this->session->get('code');
         if ($user) {
-            $user->password = $this->security->hash( $code );
+            $user->password = $this->security->hash($code);
             if ($user->save()) {
                 return true;
             }
@@ -83,9 +91,9 @@ class IndexController extends ControllerBase
             $HdUser = new HdUser();
             $HdUser->mobile = $mobile;
             $HdUser->username = $mobile;
-            $HdUser->password = $this->security->hash( $code );
-            $HdUser->update_time = date( "Y-m-d H:i:s" );
-            $HdUser->create_time = date( "Y-m-d H:i:s" );
+            $HdUser->password = $this->security->hash($code);
+            $HdUser->update_time = date("Y-m-d H:i:s");
+            $HdUser->create_time = date("Y-m-d H:i:s");
             if ($HdUser->save()) {
                 return true;
             }
