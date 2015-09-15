1f2c904e67dee8be5c8d7696a758fa1704c951cd
zsy1985104@163.com <zsy1985104@163.com>
2015-09-15 14:53:29 +0800

    商品对应车型(最底层的车)

--- src/weixin/app/controllers/MaintenanceController.php
+++ src/weixin/app/controllers/MaintenanceController.php
@@ -8,9 +8,10 @@ class MaintenanceController extends ControllerBase
         $this->view->setMainView( 'selectauto' );
 
     }
+
     public function indexAction()
     {
-        return $this->response->redirect('maintenance/autoselect');
+        return $this->response->redirect( 'maintenance/autoselect' );
     }
 
     /**
@@ -40,24 +41,41 @@ class MaintenanceController extends ControllerBase
         $brands_id = $this->request->getQuery( 'brands_id' );
         $models_id = $this->request->getQuery( 'models_id' );
 
-
+        $exact_id = $this->request->getQuery( 'exact_id' );
         $models = HdAutoModels::findFirst( array(
             "conditions" => "id = :id:",
             "bind"       => array( 'id' => $models_id )
         ) );
-        if(!$models){
-            return $this->response->redirect('maintenance/autoselect');
+        if (! $models) {
+            return $this->response->redirect( 'maintenance/autoselect' );
+        }
+        $recommend = HdAutoProductRecommend::find( array( 'conditions' => 'exact_id = :exact_id:', 'bind' => array( 'exact_id' => $exact_id ) ) );
+        $product_id_str = "";
+        $i = 0;
+        foreach ($recommend as $row) {
+            $i==0?$dian = '':$dian = ',';
+            $product_id_str .= $dian . $row->product_id;
+            $i++;
+
+        }
+        $product = HdProduct::find( array( 'conditions' => "id in ($product_id_str)" ) );
+
+        foreach ($product as $row) {
+            $category_id_array[]=  $row->category;
+        }
+        $category_id_str="";
+        $category_id_array= array_unique($category_id_array);
+        $i = 0;
+        foreach ($category_id_array as $row){
+            $i==0?$dian = '':$dian = ',';
+            $category_id_str.=$dian.$row;
+            $i++;
         }
         $brands = HdBrands::findFirst( array(
             "conditions" => "id = :id:",
             "bind"       => array( 'id' => $brands_id )
         ) );
-        $category = HdProductCategory::find(array(
-            "conditions" => "parent_id <>0"
-
-        ));
-        $product = HdProduct::find();
-
+        $category = HdProductCategory::find(array( 'conditions' => "id in ($category_id_str)" ) );
         $this->view->setVar( 'brands', $brands );
         $this->view->setVar( 'models', $models );
         $this->view->setVar( 'category', $category );
